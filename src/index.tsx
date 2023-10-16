b1f57efc4db61fe53e31825132527e564613716a
Amirmohsenjalili <amirmohsen.jalili2016@gmail.com>
2023-10-16 12:25:11 +0330

    done

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
-import styles from "./style.module.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
